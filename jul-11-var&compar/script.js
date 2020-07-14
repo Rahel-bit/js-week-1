9fe55e760bb4187fbad782bbcf14ed7b2aa0f2d6
rahel <66280581+mamush21@users.noreply.github.com>
2020-07-14 12:58:11 -0700

    practice

---
+++ jul-11-var&compar/script.js
@@ -0,0 +1,37 @@
+console.log("practice "+ 1);
+console.log(4>3);
+console.log(2 < "12");
+console.log(2.0 ==2);
+console.log(2.0 ===2);
+console.log(2.0=="2");
+console.log(2 < "John");
+console.log(2 > "John");
+console.log("2" < "2");
+console.log("2" > "12");
+console.log(1  == '1');
+console.log(1  === '1');
+console.log(0  === false);
+console.log(0  == false);
+
+
+console.log("practice "+ 2);
+
+let  a=1;
+let  b=2;
+let c = a + b;
+console.log(c);
+
+
+console.log("practice "+ 3);
+
+
+var firstName ="Rahel" ;
+var lastName = "Asmare";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+// let firstName="Rahel";
+// let lastName = firstName + " Asmare";
+// let fullName ="";
+// console.log(lastName);
